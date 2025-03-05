2467657f8fb23c7b132685df1bf1d384bf936ea1
Şahin GÖÇÜRÜCÜ <sgocurucu2259@gmail.com>
2025-03-05 13:08:14 +0300

    .gitignore updated again.

--- lib/data/ad_unit_id.dart
+++
@@ -1,5 +0,0 @@
-class AdUnitId {
-  static const String rewardedAdUnitId = "ca-app-pub-5694679104136789/7246215027";
-  static const String interstitialAdUnitId = "ca-app-pub-5694679104136789/1336284931";
-  static const String bannerAdUnitId = "ca-app-pub-5694679104136789/5354548850";
-}
